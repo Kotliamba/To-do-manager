f117d24f142eb16ece68226f8f4b5d39943233aa
Nick <spbkolia@gmail.com>
2022-02-22 22:37:21 +0300

    Initial Commit

---
+++ To do manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To do manager
+//
+//  Created by Чаусов Николай on 22.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
